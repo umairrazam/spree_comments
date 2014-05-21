99fbb49deb263f6ca7f39c8e7b6fe1cef9091e54
Jeff Dutil <JDutil@BurlingtonWebApps.com>
2014-05-20 23:07:21 -0400

    Merge branch '2-2-stable' of https://github.com/reinaris/spree_comments into 2-2-stable

--- app/overrides/add_comment_to_admin_orders_tabs.rb
+++ app/overrides/add_comment_to_admin_orders_tabs.rb
@@ -1,4 +1,4 @@
-Deface::Override.new(:virtual_path => "spree/admin/shared/_order_tabs",
+Deface::Override.new(:virtual_path => "spree/admin/shared/_order_submenu",
                     :name => "converted_admin_order_tabs_472794197",
                     :insert_bottom => "[data-hook='admin_order_tabs'], #admin_order_tabs[data-hook]",
                     :partial => "spree/admin/orders/tab",
