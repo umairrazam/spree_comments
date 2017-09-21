ad7fc8dcdb020b91d26c83be76a2e742cb6e8c2b
Bartosz Bonisławski <b.bonislawski@hotmail.com>
2017-09-21 12:52:13 +0200

    change spree version to accept 3.1+

--- db/migrate/20100406085611_create_comment_types.rb
+++ db/migrate/20100406085611_create_comment_types.rb
@@ -1,4 +1,4 @@
-class CreateCommentTypes < ActiveRecord::Migration
+class CreateCommentTypes < SpreeExtension::Migration[4.2]
   def self.up
     create_table :comment_types do |t|
       t.string :name
--- db/migrate/20111123200847_prefix_tables_with_spree.rb
+++ db/migrate/20111123200847_prefix_tables_with_spree.rb
@@ -1,6 +1,6 @@
-class PrefixTablesWithSpree < ActiveRecord::Migration
+class PrefixTablesWithSpree < SpreeExtension::Migration[4.2]
   def change
     rename_table :comments, :spree_comments
     rename_table :comment_types, :spree_comment_types
-	end
+  end
 end
