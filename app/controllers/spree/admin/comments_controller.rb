e707df52521dda016f661f38b36677e62c1a6618
Duc T. Lam <ducl_13@icloud.com>
2019-12-30 14:36:51 -0800

    Fix undefined method :back in redirect

--- app/controllers/spree/admin/comments_controller.rb
+++ app/controllers/spree/admin/comments_controller.rb
@@ -2,6 +2,7 @@ class Spree::Admin::CommentsController < Spree::Admin::ResourceController
   private
   
   def location_after_save
-    :back
+    # :back is deprecated in Rails 5, but request.referrer should also work as intended on older versions (confirmed to work on Rails 4 and 5)
+    request.referrer
   end
 end
